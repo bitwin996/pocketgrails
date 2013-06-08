527a608145669ec4ec83780b56db095ba3b73652
Mike Manila <michael.mendo_za@yahoo.com>
2013-06-08 23:49:49 +0800

    init1

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ActiveRecord::Base
   attr_accessible :content
   
   belongs_to :user
+  belongs_to :theme
   
   validates :content, presence: true, length: { maximum: 140 }
   validates :user_id, presence: true
--- app/models/theme.rb
+++ app/models/theme.rb
@@ -3,6 +3,7 @@ class Theme < ActiveRecord::Base
     
   belongs_to :category
   belongs_to :user
+  has_many :pieces, dependent: :destroy
   
   validates :user_id, presence: true
   #validates :category_id, presence: true
