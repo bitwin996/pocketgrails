abac338158b25e6e1e9c73f80c735422f4ffdbfd
Mike Manila <michael.mendo_za@yahoo.com>
2013-06-09 02:34:48 +0800

    Bug testing

--- config/environment.rb
+++ config/environment.rb
@@ -2,8 +2,4 @@
 require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
-Pocketgrails1::Application.initialize!
-
-# /config/environments.rb
-require 'aws/s3'
-AWS::S3::DEFAULT_HOST = "s3-ap-southeast-1.amazonaws.com" # if using sg buckets
+Pocketgrails::Application.initialize!
