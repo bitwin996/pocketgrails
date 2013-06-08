d88dd0070210710a92d2518e68c1f66915c68c86
Hiro <bitwin996.plus@gmail.com>
2013-06-09 05:55:44 +0800

    Modify User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
   has_secure_password
-  has_many :posts, dependent: :destroy
+  has_many :microposts, dependent: :destroy
   has_many :themes, dependent: :destroy
   has_many :categories, dependent: :destroy
+  has_many :pieces, dependent: :destroy
 
   has_many :relationships, foreign_key: "follower_id", dependent: :destroy
   has_many :followed_users, through: :relationships, source: :followed
