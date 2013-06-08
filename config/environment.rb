d7119d4ce6edb8a79f7c52fac3676f5e10fb1eaa
Mike Manila <michael.mendo_za@yahoo.com>
2013-06-09 04:48:24 +0800

    Configured s3

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Pocketgrails::Application.initialize!
+
+# /config/environments.rb
+require 'aws/s3'
+AWS::S3::DEFAULT_HOST = "s3-ap-southeast-1.amazonaws.com" # if using sg buckets
